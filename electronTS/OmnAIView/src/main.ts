24d23fa9f72b03fc9df870c448b4c8168aaa7ba9
AKMaily <anna.theimann@outlook.de>
2025-03-17 16:01:46 +0100

    Enhancement: Smaller main.ts
    Deleted the ipcMain functions as they were not used.
    Moved the backend function for the app start into backend.ts, which
    makes the code more readable and structured.

--- electronTS/OmnAIView/src/main.ts
+++ electronTS/OmnAIView/src/main.ts
@@ -1,59 +1,9 @@
 import { app, BrowserWindow, ipcMain } from "electron";
-import { spawn, ChildProcess } from "child_process";
-import * as path from "path";
-import * as fs from "fs-extra";
 import { createWindow } from "./renderer";
+import { startBackend, stopBackend } from "./backend";
 
 // Check if app was started with squirell for install process
 if (require("electron-squirrel-startup")) app.quit();
-
-let backendProcess: ChildProcess | null = null; 
-
-function getBackendPath(): string{
-  const exePath : string = app.isPackaged
-  ? path.join(process.resourcesPath, "MiniOmni.exe") // production 
-  : path.join(__dirname, "..", "res", "omnai", "MiniOmni.exe"); // development
-
-  return exePath; 
-}
-
-function startBackend(): void {
-  const exePath : string = getBackendPath(); 
-
-  if(fs.existsSync(exePath)){
-    backendProcess = spawn(exePath, ["-w"]);}
-}
-
-function stopBackend(): void {
-  if (backendProcess) {
-    backendProcess.kill();
-    console.log("Backend process stopped.");
-  }
-}
-
-// IPC Handler for backend
-ipcMain.handle("start-backend", async () => {
-  startBackend();
-  return "Backend started.";
-});
-
-ipcMain.handle("run-omnai-command", (event, commandArgs: string[]) => {
-  const exePath: string = getBackendPath(); 
-
-  const omniProcess: ChildProcess = spawn(exePath, commandArgs);
-
-  omniProcess.stdout?.on("data", (data: Buffer) => {
-    event.sender.send("omnai-output", data.toString());
-  });
-
-  omniProcess.stderr?.on("data", (data: Buffer) => {
-    event.sender.send("omnai-error", data.toString());
-  });
-
-  omniProcess.on("close", (code: number) => {
-    event.sender.send("omnai-closed", `Process exited with code ${code}`);
-  });
-});
 
 startBackend();
 
